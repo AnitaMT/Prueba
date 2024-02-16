a71d86e3f1d9605e22083cf7566329bcacd4a789
Ana Morillo Troya <amortro399@g.educaand.es>
2024-02-16 11:54:20 +0100

    primer commit

---
+++ src/ejercicioDiccionario/MiniDiccionario.java
@@ -0,0 +1,39 @@
+package ejercicioDiccionario;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MiniDiccionario {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		Map<String, String> miniDiccionario = new HashMap<String, String>();
+		
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("sol", "sun");
+		miniDiccionario.put("playa", "beach");
+		miniDiccionario.put("gato", "cat");
+		miniDiccionario.put("perro", "dog");
+		miniDiccionario.put("cena", "dinner");
+		miniDiccionario.put("rojo", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		miniDiccionario.put("ordenador", "computer");
+		
+
+	}
+
+}
